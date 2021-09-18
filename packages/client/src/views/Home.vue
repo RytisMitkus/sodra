e76ea79be087e81344ce7a35a33092b17cb548b2
RytisMitkus <rytis@mitkus.lt>
2021-09-18 20:07:25 +0300

    display data in /home

--- packages/client/src/views/Home.vue
+++ packages/client/src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <table>
+      <tr>
+        <th>Company</th>
+        <th>Code</th>
+        <th>Employees</th>
+        <th>Taxes</th>
+      </tr>
+      <tr v-for="(company, index) in companies" v-bind:key="index">
+        <td>
+          {{ company.name }}
+        </td>
+        <td>
+          {{ company.jarCode }}
+        </td>
+        <td>
+          {{ company.numInsured }}
+        </td>
+        <td>
+          {{ company.taxes }}
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      companies: [],
+    };
+  },
+  methods: {
+    async getCompanies() {
+      const companies = await axios.get("/api/data");
+      this.companies = companies.data;
+    },
+  },
+  beforeMount() {
+    this.getCompanies();
   },
 };
 </script>
+
+<style lang="sass" scoped>
+.home
+  padding: 20px
+  display: flex
+  flex-direction: column
+  align-items: center
+  justify-content: center
+  table
+    border-collapse: collapse
+    tr
+      border-bottom: 1px solid #ccc
+      &:last-child
+        border-bottom: none
+      th
+        padding: 10px
+        text-align: left
+      td
+        padding: 10px
+        text-align: left
+</style>
--- packages/server/repositories/dataRepository.js
+++ packages/server/repositories/dataRepository.js
@@ -5,7 +5,7 @@ async function getLatestData() {
     FROM companies a 
     LEFT JOIN 2021_data b ON b.jarCode = a.jarCode 
     LEFT JOIN (SELECT jarCode, max(month) as month FROM 2021_data GROUP BY jarCode) c ON c.jarCode=a.jarCode 
-    LEFT JOIN 2021_data d ON d.jarCode = a.jarCode and d.month = c.month GROUP BY 1, 2, 3;`))
+    LEFT JOIN 2021_data d ON d.jarCode = a.jarCode and d.month = c.month GROUP BY 1, 2, 3 ORDER BY taxes DESC;`))
 }
 
 module.exports = { getLatestData }
